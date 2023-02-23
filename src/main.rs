b6098563535b0fab46aac74aeb4b8bf70ea3394c
jeremymarch <jmarch@gradcenter.cuny.edu>
2023-02-23 02:19:28 -0500

    fixes missing whitespace problem: the transformed html might be correct now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use tantivy::collector::TopDocs;
 use tantivy::query::QueryParser;
 use tantivy::schema::*;
-use tantivy::{doc, Index, ReloadPolicy};
+use tantivy::{doc, Index, IndexWriter, ReloadPolicy};
 use tempfile::TempDir;
 
 use quick_xml::events::Event;
@@ -30,208 +30,221 @@ struct Lexicon {
     end_rng: u32,
 }
 
-fn read_xml(file: &str, item_count: &mut u32) {
-    //println!("file: {}", file);
-    let mut reader = Reader::from_file(file).unwrap();
-    reader.trim_text(true);
+struct Processor {
+    lexica: Vec<Lexicon>,
+}
 
-    let mut buf = Vec::new();
+impl Processor {
+    fn handle_word(&self, count: u32, lemma: &str, def: &str) {
+        println!("{} {} {}", count, lemma, def);
+    }
 
-    let mut item_text = String::from("");
-    let mut head = String::from("");
-    let mut orth = String::from("");
+    fn read_xml(&self, file: &str, item_count: &mut u32) {
+        //println!("file: {}", file);
+        let mut reader = Reader::from_file(file).unwrap();
+        reader.trim_text(false); //false to preserve whitespace
 
-    let mut sense_count = 0;
-    let mut in_orth_tag = false;
-    let mut in_head_tag = false;
-    let mut in_text_tag = false;
+        let mut buf = Vec::new();
 
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(OUTPUT)
-        .unwrap();
+        let mut item_text = String::from("");
+        let mut head = String::from("");
+        let mut orth = String::from("");
 
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-            Ok(Event::Eof) => break,
-            Ok(Event::Comment(_e)) => {}
-            Ok(Event::CData(_e)) => {}
-            Ok(Event::Decl(_e)) => {}
-            Ok(Event::PI(_e)) => {}
-            Ok(Event::DocType(_e)) => {}
+        let mut sense_count = 0;
+        let mut in_orth_tag = false;
+        let mut in_head_tag = false;
+        let mut in_text_tag = false;
 
-            Ok(Event::Start(e)) => match e.name().as_ref() {
-                b"text" => {
-                    in_text_tag = true;
-                }
-                b"head" => {
-                    in_head_tag = true;
-                }
-                b"orth" => {
-                    in_orth_tag = true;
-                    item_text.push_str(r#"<span class="orth">"#);
-                }
-                b"div1" => {
-                    head.clear();
-                    orth.clear();
-                    item_text.clear();
-                    sense_count = 0;
-                    item_text.push_str(r#"<div id=""#);
-                    let mut found_id = false;
-                    for a in e.attributes() {
-                        if a.as_ref().unwrap().key == QName(b"id") {
-                            found_id = true;
-                            item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
-                            break;
-                        }
-                    }
-                    item_text.push_str(r#"" class="body">"#);
-                    if !found_id {
-                        item_text.clear();
-                    }
-                }
-                b"div2" => {
-                    head.clear();
-                    orth.clear();
-                    item_text.clear();
-                    sense_count = 0;
-                    item_text.push_str(r#"<div id=""#);
-                    let mut found_id = false;
-                    for a in e.attributes() {
-                        if a.as_ref().unwrap().key == QName(b"id") {
-                            found_id = true;
-                            item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
-                            break;
-                        }
-                    }
-                    item_text.push_str(r#"" class="body">"#);
-                    if !found_id {
-                        item_text.clear();
-                    }
-                }
-                b"sense" => {
-                    if sense_count == 0 {
-                        item_text.push_str(r#"<br/><br/><div class="l"#);
-                    } else {
-                        item_text.push_str(r#"<br/><div class="l"#);
-                    }
-                    let mut label = String::from("");
-                    for a in e.attributes() {
-                        if a.as_ref().unwrap().key == QName(b"level") {
-                            item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
-                        } else if a.as_ref().unwrap().key == QName(b"n") {
-                            label.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
-                        }
-                    }
-                    item_text.push_str(r#"">"#);
-                    if !label.is_empty() {
-                        item_text
-                            .push_str(format!(r#"<span class="label">{}.</span>"#, label).as_str());
-                    }
-                    sense_count += 1;
-                }
-                b"author" => {
-                    item_text.push_str(r#"<span class="au">"#);
-                }
-                b"quote" => {
-                    item_text.push_str(r#"<span class="qu">"#);
-                }
-                b"foreign" => {
-                    item_text.push_str(r#"<span class="fo">"#);
-                }
-                b"i" => {
-                    item_text.push_str(r#"<span class="tr">"#);
-                }
-                b"title" => {
-                    item_text.push_str(r#"<span class="ti">"#);
-                }
-                b"bibl" => {
-                    item_text.push_str(r#"<a class="bi" biblink=""#);
-                    for a in e.attributes() {
-                        if a.as_ref().unwrap().key == QName(b"n") {
-                            item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
-                            break;
-                        }
-                    }
-                    item_text.push_str(r#"">"#);
-                }
-                _ => (),
-            },
-            Ok(Event::End(e)) => {
-                match e.name().as_ref() {
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(OUTPUT)
+            .unwrap();
+
+        loop {
+            match reader.read_event_into(&mut buf) {
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                Ok(Event::Eof) => break,
+                Ok(Event::Comment(_e)) => {}
+                Ok(Event::CData(_e)) => {}
+                Ok(Event::Decl(_e)) => {}
+                Ok(Event::PI(_e)) => {}
+                Ok(Event::DocType(_e)) => {}
+
+                Ok(Event::Start(e)) => match e.name().as_ref() {
                     b"text" => {
-                        in_text_tag = false;
+                        in_text_tag = true;
                     }
                     b"head" => {
-                        in_head_tag = false;
+                        in_head_tag = true;
                     }
                     b"orth" => {
-                        in_orth_tag = false;
-                        item_text.push_str("</span>");
+                        in_orth_tag = true;
+                        item_text.push_str(r#"<span class="orth">"#);
                     }
                     b"div1" => {
-                        item_text.push_str("</div>");
-                        //println!("item: {}", item_text);
-                        if in_text_tag && item_text.len() > 6 {
-                            //writeln!(file, "{}", item_text).unwrap();
-                        }
                         head.clear();
                         orth.clear();
                         item_text.clear();
+                        sense_count = 0;
+                        item_text.push_str(r#"<div id=""#);
+                        let mut found_id = false;
+                        for a in e.attributes() {
+                            if a.as_ref().unwrap().key == QName(b"id") {
+                                found_id = true;
+                                item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
+                                break;
+                            }
+                        }
+                        item_text.push_str(r#"" class="body">"#);
+                        if !found_id {
+                            item_text.clear();
+                        }
                     }
                     b"div2" => {
-                        item_text.push_str("</div>");
-                        //println!("item: {}", item_text);
-                        if in_text_tag && item_text.len() > 6 {
-                            *item_count += 1;
-                            writeln!(file, "{}", item_text).unwrap();
-                        }
                         head.clear();
                         orth.clear();
                         item_text.clear();
+                        sense_count = 0;
+                        item_text.push_str(r#"<div id=""#);
+                        let mut found_id = false;
+                        for a in e.attributes() {
+                            if a.as_ref().unwrap().key == QName(b"id") {
+                                found_id = true;
+                                item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
+                                break;
+                            }
+                        }
+                        item_text.push_str(r#"" class="body">"#);
+                        if !found_id {
+                            item_text.clear();
+                        }
                     }
                     b"sense" => {
-                        item_text.push_str("</div>");
+                        if sense_count == 0 {
+                            item_text.push_str(r#"<br/><br/><div class="l"#);
+                        } else {
+                            item_text.push_str(r#"<br/><div class="l"#);
+                        }
+                        let mut label = String::from("");
+                        for a in e.attributes() {
+                            if a.as_ref().unwrap().key == QName(b"level") {
+                                item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
+                            } else if a.as_ref().unwrap().key == QName(b"n") {
+                                label.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
+                            }
+                        }
+                        item_text.push_str(r#"">"#);
+                        if !label.is_empty() {
+                            item_text.push_str(
+                                format!(r#"<span class="label">{}.</span>"#, label).as_str(),
+                            );
+                        }
+                        sense_count += 1;
                     }
                     b"author" => {
-                        item_text.push_str("</span>");
+                        item_text.push_str(r#"<span class="au">"#);
                     }
                     b"quote" => {
-                        item_text.push_str("</span>");
+                        item_text.push_str(r#"<span class="qu">"#);
                     }
                     b"foreign" => {
-                        item_text.push_str("</span>");
+                        item_text.push_str(r#"<span class="fo">"#);
                     }
                     b"i" => {
-                        item_text.push_str("</span>");
+                        item_text.push_str(r#"<span class="tr">"#);
                     }
                     b"title" => {
-                        item_text.push_str("</span>");
+                        item_text.push_str(r#"<span class="ti">"#);
                     }
                     b"bibl" => {
-                        item_text.push_str("</a>");
+                        item_text.push_str(r#"<a class="bi" biblink=""#);
+                        for a in e.attributes() {
+                            if a.as_ref().unwrap().key == QName(b"n") {
+                                item_text.push_str(std::str::from_utf8(&a.unwrap().value).unwrap());
+                                break;
+                            }
+                        }
+                        item_text.push_str(r#"">"#);
                     }
                     _ => (),
+                },
+                Ok(Event::End(e)) => {
+                    match e.name().as_ref() {
+                        b"text" => {
+                            in_text_tag = false;
+                        }
+                        b"head" => {
+                            in_head_tag = false;
+                        }
+                        b"orth" => {
+                            in_orth_tag = false;
+                            item_text.push_str("</span>");
+                        }
+                        b"div1" => {
+                            item_text.push_str("</div>");
+                            //println!("item: {}", item_text);
+                            if in_text_tag && item_text.len() > 6 {
+                                //writeln!(file, "{} {}", item_count, item_text).unwrap();
+                                self.handle_word(*item_count, &head, &item_text);
+                            }
+                            head.clear();
+                            orth.clear();
+                            item_text.clear();
+                        }
+                        b"div2" => {
+                            item_text.push_str("</div>");
+                            //println!("item: {}", item_text);
+                            if in_text_tag && item_text.len() > 6 {
+                                *item_count += 1;
+                                writeln!(file, "{} {}", item_count, item_text).unwrap();
+                                self.handle_word(*item_count, &head, &item_text);
+                            }
+                            head.clear();
+                            orth.clear();
+                            item_text.clear();
+                        }
+                        b"sense" => {
+                            item_text.push_str("</div>");
+                        }
+                        b"author" => {
+                            item_text.push_str("</span>");
+                        }
+                        b"quote" => {
+                            item_text.push_str("</span>");
+                        }
+                        b"foreign" => {
+                            item_text.push_str("</span>");
+                        }
+                        b"i" => {
+                            item_text.push_str("</span>");
+                        }
+                        b"title" => {
+                            item_text.push_str("</span>");
+                        }
+                        b"bibl" => {
+                            item_text.push_str("</a>");
+                        }
+                        _ => (),
+                    }
                 }
-            }
-            Ok(Event::Empty(_e)) => {}
-            Ok(Event::Text(e)) => {
-                //txt.push(e.unescape().unwrap().into_owned())
-                // if in_head_tag {
+                Ok(Event::Empty(_e)) => {}
+                Ok(Event::Text(e)) => {
+                    //txt.push(e.unescape().unwrap().into_owned())
+                    // if in_head_tag {
 
-                // }
-                if in_head_tag {
-                    head.push_str(&e.unescape().unwrap());
+                    // }
+                    if in_head_tag {
+                        head.push_str(&e.unescape().unwrap());
+                    }
+                    if in_orth_tag {
+                        orth.push_str(&e.unescape().unwrap());
+                    }
+                    item_text.push_str(&e.unescape().unwrap());
                 }
-                if in_orth_tag {
-                    orth.push_str(&e.unescape().unwrap());
-                }
-                item_text.push_str(&e.unescape().unwrap());
             }
+            buf.clear();
         }
-        buf.clear();
     }
 }
 
@@ -247,23 +260,6 @@ fn main() -> tantivy::Result<()> {
         start_rng: 2,
         end_rng: 86,
     };
-
-    let mut item_count = 0;
-
-    if !Path::new(&lsj.dir_name).exists() {
-        println!("Cloning {}...", lsj.repo_url);
-
-        let _repo = match Repository::clone(&lsj.repo_url, &lsj.dir_name) {
-            Ok(repo) => repo,
-            Err(e) => panic!("failed to clone: {}", e),
-        };
-    }
-
-    for i in lsj.start_rng..=lsj.end_rng {
-        let path = format!("{}{}{:02}.xml", lsj.dir_name, lsj.file_name, i);
-        read_xml(&path, &mut item_count);
-    }
-    println!("items: {}", item_count);
 
     // Let's create a temporary directory for the
     // sake of this example
@@ -321,7 +317,28 @@ fn main() -> tantivy::Result<()> {
     // Here we give tantivy a budget of `50MB`.
     // Using a bigger memory_arena for the indexer may increase
     // throughput, but 50 MB is already plenty.
-    let mut index_writer = index.writer(50_000_000)?;
+    let mut index_writer: IndexWriter = index.writer(50_000_000)?;
+
+    let mut item_count = 0;
+
+    let p = Processor { lexica: vec![lsj] };
+
+    for lex in &p.lexica {
+        if !Path::new(&lex.dir_name).exists() {
+            println!("Cloning {}...", &lex.repo_url);
+
+            let _repo = match Repository::clone(&lex.repo_url, &lex.dir_name) {
+                Ok(repo) => repo,
+                Err(e) => panic!("failed to clone: {}", e),
+            };
+        }
+
+        for i in lex.start_rng..=lex.end_rng {
+            let path = format!("{}{}{:02}.xml", &lex.dir_name, &lex.file_name, i);
+            p.read_xml(&path, &mut item_count);
+        }
+        println!("items: {}", item_count);
+    }
 
     // Let's index our documents!
     // We first need a handle on the title and the body field.
